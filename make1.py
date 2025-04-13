6e1c480b15030b6df4d389bc88086992de97a827
Ben Pintof <pintobm23@juniata.edu>
2025-04-13 14:34:18 -0400

    Adding the Makefiles

---
+++ make1.py
@@ -0,0 +1,33 @@
+# Ben Pintof
+# Scientific Computing
+# Makefile Mini Project
+
+if __name__=='__main__':
+	# Creating outFile
+	outFile = open("make1.txt", "w")
+
+	# Importing Packages
+	import numpy as np
+
+	# Creating Variable
+	x = 0
+	sum = 0
+	high = 0
+	low = 10
+
+	# Creating the loop
+	for i in range(10):
+		x = np.random.randint(10)
+		sum += x
+		if x > high:
+			high = x
+		if x < low:
+			low = x
+
+	avg = sum / 10
+
+	outFile.write("Here are the Prelimary Stats for this run" + "\n")
+	outFile.write("The total sum was " + str(sum) + "\n")
+	outFile.write("The high number was " + str(high) + "\n")
+	outFile.write("The low number was " + str(low) + "\n")
+	outFile.write("The average number was " + str(avg) + "\n")
